282df404268f39e1cd8513c26b0a2b55b661fd3d
saurabh <saurabh.kumar.2382000@gmail.com>
2024-09-17 22:35:40 +0530

    replaced record with POJO class for the config

--- src/main/java/com/bank/accounts/dto/AccountsContactInfoDto.java
+++ src/main/java/com/bank/accounts/dto/AccountsContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.bank.accounts.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class AccountsContactInfoDto {
+	private String message;
+	private Map<String, String> contactDetails;
+	private List<String> onCallSupport;
 }
